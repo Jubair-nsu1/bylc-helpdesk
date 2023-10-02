96a01dd1271529d192fc343e4bad138c905d2816
Jubair Ahmed <62503637+Jubair-nsu1@users.noreply.github.com>
2023-10-03 00:09:11 +0600

    Update Form.js

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -157,7 +157,7 @@ const Form = (props) =>
           {loading && 
             // <div class="d-flex justify-content-center">Please Wait.....</div>
             <div class="d-flex justify-content-center">
-	      <h3 style={{fontWeight:'bold' }}>Processing your request...</h3>
+	      <div><h3 style={{fontWeight:'bold' }}>Processing your request...</h3></div>
               <RingLoader
                 color="#36d7b7"
                 loading={loading}
