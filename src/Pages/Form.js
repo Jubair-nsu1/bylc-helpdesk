b7b7f358d663906096c0efcb219636293c78930c
Jubair Ahmed <62503637+Jubair-nsu1@users.noreply.github.com>
2023-10-02 17:21:59 +0600

    Update Form.js

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -155,13 +155,12 @@ const Form = (props) =>
           {loading && 
             // <div class="d-flex justify-content-center">Please Wait.....</div>
             <div class="d-flex justify-content-center">
-              <RingLoader
+              <ClipLoader
                 color="#36d7b7"
                 loading={loading}
                 size={150}
                 aria-label="Loading Spinner"
                 data-testid="loader"
-                speedMultiplier={1}
               />
             </div>
           }
